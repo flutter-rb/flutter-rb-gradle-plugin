51afb87bd664f770a2b44f671cdd4b6295a39de0
fartem <jaman.smlnsk@gmail.com>
2021-05-14 20:28:42 +0300

    2021-05-14 v. 1.0.0: initial commit

---
+++ src/main/groovy/com/smlnskgmail/jaman/flutterrb/InfoPlugin.groovy
@@ -0,0 +1,24 @@
+package com.smlnskgmail.jaman.flutterrb
+
+import groovy.json.JsonOutput
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+class InfoPlugin implements Plugin<Project> {
+    @Override
+    void apply(Project project) {
+        project.task('prepareInfo') {
+            doLast {
+                def output = [
+                        'version': project.version
+                ]
+                def outputFile = new File('./flutter_rb_gradle_plugin_output.json')
+                outputFile.write(
+                        JsonOutput.prettyPrint(
+                                JsonOutput.toJson(output)
+                        )
+                )
+            }
+        }
+    }
+}
